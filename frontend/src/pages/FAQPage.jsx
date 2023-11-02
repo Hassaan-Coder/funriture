e2c006604d764b11c803aa6550dad73697f86a03
Hassaan-Coder <hassaansultan720@gmail.com>
2023-11-02 21:46:31 +0500

    update faq

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -36,8 +36,8 @@ const Faq = () => {
             className="flex items-center justify-between w-full"
             onClick={() => toggleTab(2)}
           >
-            <span className="text-lg font-medium text-gray-900">
-              Q2 . What is your return policy?
+            <span className="font-bold text-gray-900 font-Roboto">
+              Q1 . What is your return policy?
             </span>
             {activeTab === 2 ? (
               <svg
@@ -71,16 +71,17 @@ const Faq = () => {
           </button>
           {activeTab === 2 && (
             <div className="mt-4">
-              <p className="text-base text-gray-500">
-                1 .Damage During Shipping: If your order arrives with damage
-                incurred during shipping or any other damage before reaching
-                your doorstep. 2 .Item Not as Described: In the event that the
-                received item does not match the product description or is
-                significantly different from what was displayed. To initiate a
-                return, please contact us at 03003220839 with your order number
-                and a brief explanation of the issue you've encountered. We will
-                be glad to assist you in the return process in these specific
-                cases.
+              <p className="text-gray-500 font-Poppins">
+                1 . Damage During Shipping: If your order arrives damaged during
+                shipping or with any other damage before it reaches your
+                doorstep.
+              </p>
+              <p className="text-[#c56262] font-Poppins">
+                2 . Item Not as Described: If the received item doesn't match
+                the product description or significantly differs from what was
+                displayed. To start a return, please call us at 03003220839 with
+                your order number and a brief explanation of the issue. We're
+                here to help with the return process in these cases
               </p>
             </div>
           )}
@@ -91,7 +92,7 @@ const Faq = () => {
             className="flex items-center justify-between w-full"
             onClick={() => toggleTab(3)}
           >
-            <span className="text-lg font-medium text-gray-900">
+            <span className="font-bold text-gray-900 font-Roboto">
               Q2 . How do I track my order?
             </span>
             {activeTab === 3 ? (
@@ -126,12 +127,10 @@ const Faq = () => {
           </button>
           {activeTab === 3 && (
             <div className="mt-4">
-              <p className="text-base text-gray-500">
-                ANS: You can easily track your order by visiting our website and
-                logging into your account. Once logged in, go to your account
-                and find the "Track Order" option. Alternatively, you can give
-                us a call at this Number 03003220839 , and we'll be happy to
-                assist you with tracking your order..
+              <p className="text-gray-500 font-Poppins">
+                To track your order, log in to your account on our website and
+                select "Track Order." You can also call us at 03003220839, and
+                we'll assist with tracking your order.
               </p>
             </div>
           )}
@@ -142,7 +141,7 @@ const Faq = () => {
             className="flex items-center justify-between w-full"
             onClick={() => toggleTab(4)}
           >
-            <span className="text-lg font-medium text-gray-900">
+            <span className="font-bold text-gray-900 font-Roboto">
               Q3 . How do I contact customer support?
             </span>
             {activeTab === 4 ? (
@@ -177,7 +176,7 @@ const Faq = () => {
           </button>
           {activeTab === 4 && (
             <div className="mt-4">
-              <p className="text-base text-gray-500">
+              <p className="text-gray-500 font-Poppins">
                 ANS: Feel free to reach out to our customer support team by
                 calling us at 03003220839 or by messaging us on WhatsApp between
                 9 am and 11 pm, seven days a week. We are here to assist you.
@@ -191,7 +190,7 @@ const Faq = () => {
             className="flex items-center justify-between w-full"
             onClick={() => toggleTab(5)}
           >
-            <span className="text-lg font-medium text-gray-900">
+            <span className="font-bold text-gray-900 font-Roboto">
               Q4 . Can I change or cancel my order?
             </span>
             {activeTab === 5 ? (
@@ -226,7 +225,7 @@ const Faq = () => {
           </button>
           {activeTab === 5 && (
             <div className="mt-4">
-              <p className="text-base text-gray-500">
+              <p className="text-gray-500 font-Poppins">
                 ANS: You can change or cancel your order by calling us at
                 03003220839 within 24 hours of placing it. After this 24-hour
                 window, changes or cancellations will not be possible.
@@ -240,7 +239,7 @@ const Faq = () => {
             className="flex items-center justify-between w-full"
             onClick={() => toggleTab(7)}
           >
-            <span className="text-lg font-medium text-gray-900">
+            <span className="font-bold text-gray-900 font-Roboto">
               Q5 . How can I make a payment?
             </span>
             {activeTab === 7 ? (
@@ -275,12 +274,11 @@ const Faq = () => {
           </button>
           {activeTab === 7 && (
             <div className="mt-4">
-              <p className="text-base text-gray-500">
-                ANS: We accept payments via bank accounts and also offer a Cash
-                on Delivery (COD) option. For COD orders, we'll contact you via
-                the phone number you provide to arrange an online advance
-                payment through a bank account. The rest can be paid during COD
-                delivery.
+              <p className="text-gray-500 font-Poppins">
+                We accept payments through bank accounts and also offer Cash on
+                Delivery (COD). For COD orders, we'll contact you via the phone
+                number you provide to arrange an advance payment through a bank
+                account, with the remainder payable during COD delivery.
               </p>
             </div>
           )}
