0831c67c42624b999d8570462ed4f069272bdcb4
Hassaan-Coder <hassaansultan720@gmail.com>
2023-11-09 22:05:53 +0500

    testing

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ cloudinary.config({
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:8000`);
 });
 
 // unhandled promise rejection
