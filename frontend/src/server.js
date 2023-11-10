148f0bde752dfd845c9fd4207cc68d773f574d36
Hassaan-Coder <hassaansultan720@gmail.com>
2023-11-10 11:14:26 +0500

    checking 2 url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-// export const server = "https://funritures.vercel.app/api/v2";
-export const server = "http://api.interior-impression.com/api/v2";
+export const server = "https://funritures.vercel.app/api/v2";
+// export const server = "http://api.interior-impression.com/api/v2";
